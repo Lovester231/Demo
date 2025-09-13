65596c16e0c2004d3874fa041e5ba33f9d125643
Lovester231 <unstoppabletlove@gmail.com>
2025-09-13 08:06:32 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { BettingApp } from "./components/BettingApp";
+
+function App() {
+  return <BettingApp />;
+}
+
+export default App;
---
+++ BettingApp.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+export function BettingApp() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1 style={{ color: "lime" }}>Betting Demo (Practice Only)</h1>
+      <p>This is a demo version with fixtures, results, and betslip.</p>
+    </div>
+  );
+}
